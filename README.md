6fcce1bced99f4abd27f1c58407670495ce2a2e1
Harshitha Bandaru <harshitha4547@gmail.com>
2024-12-26 02:57:48 +0530

    Initial Project Structure

---
+++ README.md
@@ -0,0 +1,6 @@
+DevTinder
+
+1. Creating the Project
+
+- npm init - generates package.json
+- src/app.js - starting point of application
---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+// Request Handlers
+// Home route
+app.use((req, res) => {
+  res.send("Hello from home page");
+});
+
+// build a specific route
+app.use("/test", (req, res) => {
+  res.send("Hello from test route");
+});
+
+// listen on a port
+app.listen(3000, () => {
+  console.log("Server Started Succcessfully");
+});
