93b8defbdd0dcb58f050f0a1bdd6bcc6974cdde9
Harshitha Bandaru <harshitha4547@gmail.com>
2024-12-29 23:29:49 +0530

    use,get,post,dynamic ids, query params

--- src/app.js
+++ src/app.js
@@ -12,6 +12,29 @@ app.use("/test", (req, res) => {
   res.send("Hello from test route");
 });
 
+app.use("/testUser", (req, res) => {
+  res.send("Hello from test user route");
+});
+
+app.get("/getUser", (req, res) => {
+  res.send("getuser");
+});
+
+app.get("/getUserwithID/:id", (req, res) => {
+  // getting dynamic id
+  console.log(`${req.params} user is Harshi`);
+  console.log("req.params", req.params.id);
+  res.status(200);
+  res.send("Exists");
+});
+
+app.post("/postUser", (req, res) => {
+  // query params
+  console.log("req.query", req.query.userName);
+  console.log("req.query", req.query.email);
+  res.send(`${req.query.userName} data posted successfully`);
+});
+
 // listen on a port
 app.listen(3000, () => {
   console.log("Server Started Succcessfully");
